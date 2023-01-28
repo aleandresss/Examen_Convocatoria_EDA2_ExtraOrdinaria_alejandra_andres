5178879dc56a8eb083f07c5ab8d114660a249dfb
aleandresss <114179514+aleandresss@users.noreply.github.com>
2023-01-28 09:30:55 +0100

    constructor + "la armadura se ha creado con exito"

---
+++ armaduras.py
@@ -0,0 +1,10 @@
+from random import randit 
+
+class armadura(object):
+    nombre = ""
+    rango = ""
+
+    def _init_ (self,nombre,rango):
+        self.nombre = nombre
+        self.rango = rango 
+        print("La armadura se ha creado con exito")
--- ejercicio6.py
+++ ejercicio6.py
@@ -8,8 +8,28 @@ class Mochila:
     def __lt__ (self,otro):
         return self.report and __builtins__
     def valor_maximo(peso, valores, capacidad):
-        corto = []
+            corto = []
         for i in range(len (peso)):
             corto.append (Mochila(peso[i],valores[i], i))
         
         corto.sort (reverse = True)
+
+        contarvalor = 0
+            for objeto in corto:
+                pesoactual = int(objeto.peso)
+                valoractual = int(objeto.valor)
+                if capacidad - pesoactual>= 0:
+                    capacidad -= pesoactual
+                    contarvalor += valoractual
+                else:
+                    fracción = capacidad/pesoactual
+                    contarvalor += valoractual*fracción
+                    capacidad = int(capacidad - (pesoactual*fracción))
+                    break 
+                return contravalor 
+
+precio = [103, 60, 70, 5, 15] 
+valores = [12, 23, 11, 15, 7]
+capacidad = 100
+maxvalor = valor_maximo(peso, valores, capacidad)
+print ("el valor máximo que se permite en la mochila es =", maxvalor) 
